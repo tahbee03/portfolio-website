301ab54eeb35627d05b305a1131774f10e78f346
talikeb <talikeb20@gmail.com>
2022-09-26 15:24:49 -0400

    used Jinja to establish a template and created the other webpages

--- app/__init__.py
+++ app/__init__.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/about-me/")
+def about_me():
+    return render_template("about_me.html")
+
+@app.route("/mini-comics/")
+def mc():
+    return render_template("mc.html")
+
+@app.route("/projects/")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/art/")
+def art():
+    return render_template("art.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
