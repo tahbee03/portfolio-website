741d96ea8d23ab544fc28a8d1eb471839ded655e
talikeb <talikeb20@gmail.com>
2022-12-18 01:19:32 -0500

    updated design + functionality - added in placeholder cards for experiences for About Me page - elongated bio for About Me page - removed the trailing slash for routes - changed CSS link section for MC, home, and contact pages

--- app/__init__.py
+++ app/__init__.py
@@ -6,23 +6,23 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/about-me/")
+@app.route("/about-me")
 def about_me():
     return render_template("about_me.html")
 
-@app.route("/mini-comics/")
+@app.route("/mini-comics")
 def mc():
     return render_template("mc.html")
 
-@app.route("/projects/")
+@app.route("/projects")
 def projects():
     return render_template("projects.html")
 
-@app.route("/art/")
+@app.route("/art")
 def art():
     return render_template("art.html")
 
-@app.route("/contact/")
+@app.route("/contact")
 def contact():
     return render_template("contact.html")
 
