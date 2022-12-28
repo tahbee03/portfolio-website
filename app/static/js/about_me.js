83a4487d27a32d6f9b5340f90379be91e867121c
talikeb <talikeb20@gmail.com>
2022-12-28 00:15:52 -0500

    updated design and project structure - moved JS functionalities from HTML files to separate JS files - added content to the Projects page - updated README.md

---
+++ app/static/js/about_me.js
@@ -0,0 +1,30 @@
+/*
+HELPFUL RESOURCES:
+https://codepen.io/bstonedev/pen/MWWZgKz
+https://www.w3schools.com/jsref/met_document_addeventlistener.asp
+https://www.w3schools.com/jsref/met_element_getboundingclientrect.asp
+https://www.w3schools.com/jsref/prop_win_innerheight.asp
+https://www.w3schools.com/jsref/prop_element_classlist.asp
+*/
+
+let elements = document.querySelectorAll(".card");
+
+window.addEventListener('load', () => {
+    for(var i = 0; i < elements.length; i++) {
+        var elem = elements[i];
+        var dist = elem.getBoundingClientRect().top - window.innerHeight + 25;
+
+        if(dist < 0) elem.classList.add("in-view");
+        else elem.classList.remove("in-view");
+    }
+});
+
+window.addEventListener('scroll', () => {
+    for(var i = 0; i < elements.length; i++) {
+        var elem = elements[i];
+        var dist = elem.getBoundingClientRect().top - window.innerHeight + 50;
+
+        if(dist < 0) elem.classList.add("in-view");
+        else elem.classList.remove("in-view");
+    }
+});
---
+++ app/static/js/projects.js
@@ -0,0 +1,21 @@
+let elements = document.querySelectorAll(".card");
+
+window.addEventListener('load', () => {
+    for(var i = 0; i < elements.length; i++) {
+        var elem = elements[i];
+        var dist = elem.getBoundingClientRect().top - window.innerHeight + 25;
+
+        if(dist < 0) elem.classList.add("in-view");
+        else elem.classList.remove("in-view");
+    }
+});
+
+window.addEventListener('scroll', () => {
+    for(var i = 0; i < elements.length; i++) {
+        var elem = elements[i];
+        var dist = elem.getBoundingClientRect().top - window.innerHeight + 50;
+
+        if(dist < 0) elem.classList.add("in-view");
+        else elem.classList.remove("in-view");
+    }
+});
