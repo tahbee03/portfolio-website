3cbebeac2a32c7dae9a646e52dd94854b8af3e47
talikeb <talikeb20@gmail.com>
2023-03-03 02:16:30 -0500

    added picture easter egg on About Me page

--- static/js/about_me.js
+++ static/js/about_me.js
@@ -30,3 +30,38 @@ window.addEventListener('scroll', () => {
         else elem.classList.remove("in-view");
     }
 });
+
+
+
+// Flip-and-change animation for bio picture
+
+/*
+HELPFUL RESOURCE(S):
+https://stackoverflow.com/questions/19799846/javascript-rotate-img-on-click
+https://stackoverflow.com/questions/7882356/javascript-get-img-src-and-set-as-variable
+https://developer.mozilla.org/en-US/docs/Web/CSS/transform-function/rotateY
+*/
+
+var picture = document.getElementById("bio-pic");
+
+picture.addEventListener('click', () => {
+
+    picture.classList.add("rotated-image"); // Add class to class list of picture to apply effect
+
+    setTimeout(() => {
+        // Change picture source accordingly
+        if(picture.getAttribute("src") == "/static/img/talike1.jpg") {
+            picture.src = "/static/img/code_pfp.png";
+        } else {
+            picture.src = "/static/img/talike1.jpg";
+        }
+        picture.classList.remove("rotated-image"); // Remove class from class list of picture to remove effect 
+    }, 500);
+
+    // Since the picture's transition is set to 0.5 sec, that means it will take 0.5 sec for the effect
+    // to be applied. Setting the time interval in setTimeout() to the same value gives the illusion
+    // that the website "waits" for the transition to finish before changing the picture. Removing
+    // the rotated-image class inside of setTimeout() also allows a clean transition back to the
+    // original rotation of the picture instead of not showing a transition at all.
+
+})
