5a88d75d4c58232943a2843a06eb6ad80fa247df
talikeb <talikeb20@gmail.com>
2023-04-22 14:32:11 -0400

    used Flask and Jinja to make page title, description, CSS, and JS accessible in main.html

--- __init__.py
+++ __init__.py
@@ -5,27 +5,50 @@ app = Flask(__name__) # Instantiates Flask app
 # Home page
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template(
+        "home.html", 
+        title="Talike's Portfolio", 
+        desc="This website is a portfolio of work done by me",
+        css="home",
+        js="home")
 
 # About Me page
 @app.route("/about-me")
 def about_me():
-    return render_template("about_me.html")
+    return render_template(
+        "about_me.html", 
+        title="About Me", 
+        desc="A little bit about me, my education, and my extracurricular experience",
+        css="about_me",
+        js="about_me")
 
 # Mini Comics page
 @app.route("/mini-comics")
 def mc():
-    return render_template("mc.html")
+    return render_template(
+        "mc.html", 
+        title="Mini Comics", 
+        desc="Comics galore!",
+        css="mc")
 
 # Projects page
 @app.route("/projects")
 def projects():
-    return render_template("projects.html")
+    return render_template(
+        "projects.html", 
+        title="Projects", 
+        desc="A list of my best projects over the years",
+        css="projects",
+        js="projects")
 
 # Contact page
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template(
+        "contact.html", 
+        title="Contact", 
+        desc="All of my social media",
+        css="contact")
 
 # Runs Flask app
 if __name__ == "__main__":
