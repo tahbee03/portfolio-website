46ed9ad040c9e52ac9642df8c2c045662a6fa885
talikeb20@gmail.com <talikeb20@gmail.com>
2023-12-30 17:49:32 -0500

    changed images for link previews

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def home():
         desc="This website is a portfolio of work done by me",
         css="home",
         js="home",
-        page_img=f"{img_dir}talike5.jpg")
+        page_img=f"{img_dir}talike9.jpg")
 
 # About Me page
 @app.route("/about-me")
@@ -24,7 +24,7 @@ def about_me():
         desc="A little bit about me, my education, and my extracurricular experience",
         css="about_me",
         js="about_me",
-        page_img=f"{img_dir}talike7.jpg")
+        page_img=f"{img_dir}talike10.jpg")
 
 # Mini Comics page
 @app.route("/mini-comics")
@@ -35,7 +35,7 @@ def mc():
         desc="Comics galore!",
         css="mc",
         js="mc",
-        page_img=f"{img_dir}MC1_1.png")
+        page_img=f"{img_dir}tpmc_logo.jpg")
 
 # Route to return the number of pictures for a specific comic
 @app.route("/mc_list/<num>")
@@ -51,7 +51,7 @@ def projects():
         desc="A list of my best projects over the years",
         css="projects",
         js="projects",
-        page_img=f"{img_dir}catch_the_ball.png")
+        page_img=f"{img_dir}itz.png")
 
 # Runs Flask app
 if __name__ == "__main__":
